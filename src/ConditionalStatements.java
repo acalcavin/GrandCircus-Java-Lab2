d25f61346127be00c403b6fd484e83372b43d5c9
aicavin <aicavin@gmail.com>
2018-01-23 16:22:46 -0500

    add parameters, userName, and number validation

--- src/ConditionalStatements.java
+++ src/ConditionalStatements.java
@@ -10,50 +10,69 @@ public class ConditionalStatements {
 	public static void main(String[] args) {
 
 		int userInput;
-		char userChar = 'y';  //set initial condition as yes
+		char userChar = 'y'; // set initial condition as yes
+		String userName;
 
 		Scanner scnr = new Scanner(System.in);
 
+		System.out.println("Hi! What is your name?");
+		userName = scnr.next();
+
+		System.out.println("Ok, " + userName + "!");
+		
 		while ((userChar == 'y') || (userChar == 'Y')) { // execute if yes
 
 			System.out.println("Please enter an integer between 1 and 100: ");
 
 			userInput = scnr.nextInt();
 
-			if ((userInput % 2) == 0) { // check if even
-
-				if ((userInput >= 2) && !(userInput > 25)) {
-					System.out.println("Even and less than 25");
-				}
-
-				else if ((userInput >= 26) && !(userInput > 59)) {
-					System.out.println("Even");
-				} else if (userInput >= 60) {
-					System.out.println(userInput + " Even");
-				}
-				System.out.println();
-			}
-
-			else { 
-
-				System.out.print(userInput + " Odd");
-
-				if (userInput > 60) {
-					System.out.print(" and over 60.");
-				}
+			if ((userInput < 1) || (userInput > 100)) {
 				
-				System.out.println("\n");
+				System.out.println("Come on, " + userName + "!");
 			}
+			
+			else {
 
-			System.out.println("Continue? (y/n)");
-			userChar = scnr.next().charAt(0);
+				if ((userInput % 2) == 0) { // check if even
 
-			System.out.println();
+					if ((userInput >= 2) && !(userInput > 25)) {
+						System.out.println("Even and less than 25");
+					}
 
-			if ((userChar == 'n') || (userChar == 'N')) {
-				System.out.println("Goodbye!");
+					else if ((userInput >= 26) && !(userInput > 59)) {
+						System.out.println("Even");
+					} else if (userInput >= 60) {
+						System.out.println(userInput + " Even");
+					}
+					System.out.println();
+				}
+
+				else {
+
+					System.out.print(userInput + " Odd");
+
+					if (userInput > 60) {
+						System.out.print(" and over 60.");
+					}
+
+					System.out.println("\n");
+				}
+
+				System.out.println(userName + ", would you like to continue? (y/n)");
+				userChar = scnr.next().charAt(0);
+
+				System.out.println();
+
+				if ((userChar == 'n') || (userChar == 'N')) {
+					System.out.println("So long, " + userName + "!");
+				}
 			}
+			
+			/*
+			else {
+				System.out.println("Come on, " + userName + ", that is not between 1 and 100!");
+			}
+			*/
 		}
-
 	}
 }
